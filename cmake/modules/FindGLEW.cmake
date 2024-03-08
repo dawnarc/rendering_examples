8a9cf2f31effd97cf256a1484b68a2665af146f5
dawnarc.com <aiguo1024@gmail.com>
2024-03-08 15:03:24 +0800

    suppress warning on debug config - LNK4098: defaultlib 'MSVCRT' conflicts with use of other libs

--- cmake/modules/FindGLEW.cmake
+++ cmake/modules/FindGLEW.cmake
@@ -17,13 +17,13 @@ set( _glew_HEADER_SEARCH_DIRS
 "/usr/include"
 "/usr/local/include"
 "${CMAKE_SOURCE_DIR}/includes"
-"D:/Program_Filesx86/glew/include" )
+"C:/Program Files (x86)/glew/include" )
 
 set( _glew_LIB_SEARCH_DIRS
 "/usr/lib"
 "/usr/local/lib"
 "${CMAKE_SOURCE_DIR}/lib"
-"D:/Program_Filesx86//glew/lib" )
+"C:/Program Files (x86)//glew/lib" )
 
 # Check environment for root search directory
 set( _glew_ENV_ROOT $ENV{glew_ROOT} )
